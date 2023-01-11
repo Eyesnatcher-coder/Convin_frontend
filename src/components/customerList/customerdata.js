468978c4f5aa207953f25203fe5a387c872c5afa
Harsh Kanojia <96467990+Eyesnatcher-coder@users.noreply.github.com>
2023-01-12 02:12:54 +0530

    *

--- src/components/customerList/customerdata.js
+++ src/components/customerList/customerdata.js
@@ -1,7 +1,7 @@
 import axios from "axios";
-setTimeout(function () { 
-    window.location.reload();
-  }, 12 *1000);
+// setTimeout(function () { 
+//     window.location.reload();
+//   }, 12 *1000);
 const fetch_logins = async () => {
     const res = await axios.get(process.env.REACT_APP_API_URL + "/yourbuckets")
     return res.data;
